e6f5392310f0e6b77fb0a32cf39228c71bbeeb27
John Montesano <jmontesano7@gmail.com>
2017-10-05 21:57:12 -0400

    Had an issue where I thought the syscall was a module so I changed it to only have necessary code

--- kernelspace/sysCallTest.c
+++ kernelspace/sysCallTest.c
@@ -2,29 +2,13 @@
 #include <linux/module.h>
 #include<linux/linkage.h>
 
-MODULE_LICENSE("DUAL BSD/GPL");
-
-// called when module is installed
-int __init init_module()
-{
-	printk(KERN_ALERT "my_xtime: Oh you know its in there my friend!\n");
-
-	return 0;
-}
+//MODULE_LICENSE("DUAL BSD/GPL");
 
 asmlinkage int sys_my_xtime(struct timespec *current_time)
 {
-	printk(KERN_ALERT "my_xtime:Oh great job calling the system call!\n");
+	printk(KERN_ALERT "my_xtime: my_xtime was called!\n");
 	return -EFAULT;
 }
 
-EXPORT_SYMBOL(sys_my_xtime);
-
-
-
-// called when module is removed
-void __exit cleanup_module()
-{
-	printk(KERN_ALERT "my_xtime: im out bud!!\n");
-}
+//EXPORT_SYMBOL(sys_my_xtime);
 
