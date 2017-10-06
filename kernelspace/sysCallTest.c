c349ab5c85648fae5cb1dcec3edd09e7f5478222
John Montesano <jmontesano7@gmail.com>
2017-10-05 22:42:37 -0400

    actually added the functionality to move timespec from user to kernel back to user, changed both user and kernel files to do so

--- kernelspace/sysCallTest.c
+++ kernelspace/sysCallTest.c
@@ -1,13 +1,37 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include<linux/linkage.h>
+#include <linux/linkage.h>
+#include <linux/time.h>
+#include <linux/uaccess.h>
 
 //MODULE_LICENSE("DUAL BSD/GPL");
 
 asmlinkage int sys_my_xtime(struct timespec *current_time)
 {
+	struct timespec buff;
 	printk(KERN_ALERT "my_xtime: my_xtime was called!\n");
-	return -EFAULT;
+
+
+/*
+	if(access_ok(VERIFY_WRITE, current_time, sizeof(buff))
+	{
+		printk(KERN_ALERT "my_xtime: FAILED, issue with memory given!\n");
+		return -EFAULT;
+	}
+*/	
+
+
+	if(copy_from_user(&buff, current_time, sizeof(buff)))
+	{
+		return -EFAULT;
+	}
+	buff = current_kernel_time();
+	if(copy_to_user(current_time, &buff, sizeof(buff)))
+	{
+		return -EFAULT;
+	}
+	return 0;
+	printk(KERN_ALERT "my_xtime: SUCCESFUL COPY!\n");
 }
 
 //EXPORT_SYMBOL(sys_my_xtime);
--- userspace/user.c
+++ userspace/user.c
@@ -2,12 +2,27 @@
 #include <errno.h>
 #include <unistd.h>
 #include <linux/unistd.h>
+#include <time.h>
 
 #define __NR_sys_my_xtime 326
 
 int main(void)
 {
-	int ret = syscall(__NR_sys_my_xtime);
+	//int ret = syscall(__NR_sys_my_xtime);
+	//printf("ret=%d errno=%d \n",ret,errno);
+	struct timespec  t;
+	t.tv_sec = -1;
+	t.tv_nsec = -1;
+	printf("BEFORE SYSCALL: Current Time =  %ld seconds and %ld nanoseconds \n", t.tv_sec, t.tv_nsec); 
+	int ret = syscall(__NR_sys_my_xtime, t);
+	printf("AFTER SYSCALL: Current Time =  %ld seconds and %ld nanoseconds \n", t.tv_sec, t.tv_nsec); 
 	printf("ret=%d errno=%d \n",ret,errno);
+
+//	USED TO FIGURE OUT SIZE OF FILLED TIMESPEC
+
+//	t.tv_sec = 10000;
+//	t.tv_nsec = 888;
+//	printf("Size of timespec: %ld ", sizeof(*t)); 
+
 	return 0;
 }
